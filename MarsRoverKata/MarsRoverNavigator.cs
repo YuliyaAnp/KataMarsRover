b52aad5924b7aa7cc7128f6583fbdc6be276c2a8
Yuliya Anpilova <yuliya.anpilova@asos.com>
2018-03-02 17:33:46 +0000

    initial commit

---
+++ MarsRoverKata/MarsRoverNavigator.cs
@@ -0,0 +1,10 @@
+﻿namespace MarsRoverKata
+{
+    public class MarsRoverNavigator
+    {
+        public static string Navigate(string startingDirection, string inputeControlSymbol)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ MarsRoverKataTests/MarsRoverNavigatorShould.cs
@@ -0,0 +1,22 @@
+﻿using MarsRoverKata;
+using NUnit.Framework;
+
+namespace MarsRoverKataTests
+{
+    [TestFixture]
+    public class MarsRoverNavigatorShould
+    {
+        [Test]
+        public void ReturnEastWhenInputIsLeftAndStateIsNorth()
+        {
+            string startingDirection = "N";
+            string inputControlSymbol = "L";
+
+            string expected = "E";
+
+            string actual = MarsRoverNavigator.Navigate(startingDirection, inputControlSymbol);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
